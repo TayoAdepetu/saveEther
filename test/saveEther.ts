d2834e5d3a8732fbd03eef4502e74d3fac8cf752
Tayo <copyafrica93@gmail.com>
2024-02-15 14:20:57 +0000

    testing commit

--- test/saveEther.ts
+++ test/saveEther.ts
@@ -1,33 +1,68 @@
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { expect } from "chai";
 import { ethers } from "hardhat";
+import { expect } from "chai";
 
-describe("TodoAppTest", function () {
-  async function deployTodoApp() {
-    const Todo = await ethers.getContractFactory("TodoApp");
-    const todo = await Todo.deploy();
+describe("SaveEtherTest", function () {
+  async function deploySaveEther() {
+    const SaveEther = await ethers.getContractFactory("SaveEther");
+    const saveEther = await SaveEther.deploy();
+    await saveEther.deployed();
 
-    return { todo };
+    return { saveEther };
+
+    // [owner, receiver] = await ethers.getSigners();
   }
 
-  describe("setTodo", function () {
-    it("Should create new Todo", async function () {
-      const { todo } = await loadFixture.deployTodoApp();
+  describe(""),
+    function () {
+      it("should deposit and check savings", async function () {
+        const { saveEther } = await deploySaveEther();
+        const tx = await saveEther.deposit(1);
 
-      const tx = await todo.setTodo(
-        "My First Todo Item",
-        "This is my very first todo item in 2024"
-      );
-      const TODO = await todo.getMyTodos();
+        // Wait for transaction to be mined
+        await tx.wait();
 
-      expect(TODO).to.equal({
-        user_id: 0x5b38da6a701c568545dcfcb03fcb875f56beddc4,
-        todoID: 1,
-        title: "My First Todo Item",
-        description: "This is my very first todo item in 2024",
-        isDone: false,
-        status: 0,
+        const address = "checkingCaller"; //this is just a placeholder
+
+        const savings = await saveEther.checkSavings(address);
+
+        expect(savings).to.equal(1, "Deposit amount not saved correctly");
       });
-    });
-  });
+
+      it("should deposit, withdraw and check savings", async function () {
+        //First deposit
+        const { saveEther } = await deploySaveEther();
+        const tx = await saveEther.deposit(1);
+        await tx.wait();
+
+        //Now, wothdraw part of it
+        const Withdraw = await saveEther.withdraw(0.5);
+        await Withdraw.wait();
+
+        //Now, check balance
+        const address = "checkingCaller"; //this is just a placeholder
+        const savings = await saveEther.checkSavings(address);
+        expect(savings).to.equal(0.5, "Savings not correct after withdrawal");
+      });
+
+      it("should deposit, send out savings, and check receiver savings", async function () {
+        //First deposit
+        const { saveEther } = await deploySaveEther();
+        const tx = await saveEther.deposit(1);
+        await tx.wait();
+
+        //Now, send out savings
+        const receiver_address = "checkingCaller"; //this is just a placeholder
+        const Sending = await saveEther.sendOutSaving(receiver_address, 0.4);
+        await Sending.wait();
+
+        //Now, check balance
+        const address = "checkingCaller"; //this is just a placeholder
+        const savings = await saveEther.checkSavings(receiver_address);
+        expect(savings).to.equal(
+          0.6,
+          "Receiver savings not correct after sending out savings"
+        );
+      });
+    };
 });
